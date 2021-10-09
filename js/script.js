b2b1d0ff23ae19cd69b40b32b3143180ae750dfd
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-10-09 16:58:08 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/*//1.
-//let a = +prompt("введите число");
+//1.
+let a = +prompt("введите число");
 let a = +prompt("введите число");
     a === 0 ? alert('верно') :  alert('неверно')
 //2.
@@ -25,10 +25,51 @@ let a = prompt("введите данные");
     a === '1' ? alert('верно') :  alert('неверно')
 //9.
 let a = prompt("введите данные");
-    5 > a > 0  ? alert('верно') :  alert('неверно')*/
+    5 > a > 0  ? alert('верно') :  alert('неверно')
 //10.
 let a = +prompt('введите число');
-//let result;
-    if (a === 0 || a === 2) {
-        alert(${'a'} + 7 )
+
+  if (a === 0 || a === 2) {
+    alert(a + 7 );
+  } else {
+      alert(a / 10);
     }
+//11.
+
+let a = +prompt('введите число');
+let b = +prompt('введите число');
+
+  if (a <= 1 && b >= 3) {
+    alert (a + b);
+  } else {
+      alert (a - b);
+  }
+//12.
+
+let a = +prompt('введите число');
+let b = +prompt('введите число');
+
+  if (11 > a > 2 || (14 > b >= 6)) {
+    alert('верно');
+  } else {
+      alert('неверно');
+  }
+//13.
+let num = +prompt('введите число');
+let result;
+  switch (num) {
+    case 1:
+      alert(result='зима');
+      break;
+    case 2:
+      alert(result='весна');
+      break;
+    case 3:
+      alert(result ='лето');
+      break;
+    case 4:
+      alert(result ='осень');
+      break;
+    default:
+      alert('неизвестное значение');
+  }
